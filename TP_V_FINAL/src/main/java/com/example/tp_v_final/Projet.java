7a6cd1c1132419598e860661e01f22c6c85d3813
amourayusra <96579973+amourayusra@users.noreply.github.com>
2023-05-18 14:49:35 +0100

    h

---
+++ TP_V_FINAL/src/main/java/com/example/tp_v_final/Projet.java
@@ -0,0 +1,4 @@
+package com.example.tp_v_final;
+
+public class Projet {
+}
--- TP_V_FINAL/src/main/java/com/example/tp_v_final/User.java
+++ TP_V_FINAL/src/main/java/com/example/tp_v_final/User.java
@@ -1,4 +1,25 @@
 package com.example.tp_v_final;
 
+
+
 public class User {
+    private Calendrier calendrier;
+    private Historique historique ;
+    private Tache[] taches_non_panifiées;
+    private Badge[] badges;
+    private Projet[] projet;
+
+
+    public void planifier(Tache t){}
+    public void planifierP(Projet p){}
+    public void valider(){}
+    public void replanifier ( Tache t){}
+    public void fixerPeriode(float hi, float hf){}
+    public void supprimer(Tache t ){}
+    public void annuler(){}
+    public void ajouterProjet(){}
+    public float calculRendement(){ return 0;}
+
+
+
 }
